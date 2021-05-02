445eea695649f9ea4409436d91d60e3a1b1d062f
Mohammed Ouedrhiri <77699641+Mouedrhiri@users.noreply.github.com>
2021-05-02 15:31:46 +0000

    Update progressbar.py

--- progressbar.py
+++ progressbar.py
@@ -1,8 +1,5 @@
-#I've Used tqdm library to make the progress Bar
 from tqdm import tqdm
-#I used Time Library to Control The Speed and The Progress
 from time import sleep
-#The Description It The Text Showen in the left of The ProgressBar
 for i in tqdm(range(0, 100), desc ="Progress : "):
         sleep(.1)
 
